94d5351e4ff9d7ca6608517795c9ba42488d801e
Omurbek Dulatov <oma.dulatov@gmail.com>
2023-04-15 14:58:20 +0600

    Added new chapters of docs

--- apps/polls/urls.py
+++ apps/polls/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-# from apps.polls.views import polls
-from apps.polls.views import PollsView
+from . import views
+
+app_name = 'polls'
 
 urlpatterns = [
-    # path('', polls, name='index')
-    path('', PollsView.as_view(), name='polls')
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
